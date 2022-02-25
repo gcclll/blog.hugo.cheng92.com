9a584b1a5e516231cdc92261ded15e7151a2f754
gcclll <gccll.love@gmail.com>
2022-02-25 11:25:41 +0800

    2022-02-25 11:25:41

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,7 +7,7 @@ for file in $(ls $src); do
     css="$target/${file%.*}.css"
     echo "$css"
     sass $src/$file "$css"
-    sed -i -e '1i\/*请勿手动修改该文件，它是由 sass 编译生成的。*/' "$css" >"$css"
+    sed -i -e '1i/*请勿手动修改该文件，它是由 sass 编译生成的。*/' "$css" >"$css"
 done
 
 cd $path
