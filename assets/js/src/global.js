97d74e42ccf2943fab7a885f04450a25919f0925
gcclll <gccll.love@gmail.com>
2022-02-26 01:49:32 +0800

    2022-02-26 01:49:32

--- assets/js/src/global.js
+++ assets/js/src/global.js
@@ -40,7 +40,7 @@ $(function () {
 <el-input v-model="search" placeholder="请输入搜索内容(仅限本文，TODO搜索全博客)"/>
 <ul style="max-height:500px;overflow-y:scroll;text-align:left">
   <li v-for="(result, i) in filterResults" :key="result.value">
-    <a :href="result.link">{{result.value}}</a>
+    <a @click="handleClose" :href="result.link">{{result.value}}</a>
   </li>
 </ul>
 </el-dialog>
