301d8f1d60f540c9e6d1f02df0baab625b592402
gcclll <gccll.love@gmail.com>
2022-03-01 10:48:12 +0800

    2022-03-01 10:48:12

--- assets/js/dist/global.js
+++ assets/js/dist/global.js
@@ -29,7 +29,8 @@ $(function () {
   var searchTmpl = "<div id=\"search\">Loading...</div>";
 
   if (isHome) {
-    // 收集所有标题(id包含 'outline-container-' 且以它开头的 div)
+    $('#table-of-contents').hide(); // 收集所有标题(id包含 'outline-container-' 且以它开头的 div)
+
     $("<div id=\"vue-toc\"></div>").insertAfter('#content>h1');
     $('h1.title').append(searchTmpl);
     var outlines = findOutlines();
--- assets/js/src/global.js
+++ assets/js/src/global.js
@@ -23,6 +23,7 @@ $(function () {
   const isHome = /home\.html$/.test(location.pathname)
   const searchTmpl = `<div id="search">Loading...</div>`
   if (isHome) {
+    $('#table-of-contents').hide()
     // 收集所有标题(id包含 'outline-container-' 且以它开头的 div)
     $(`<div id="vue-toc"></div>`).insertAfter('#content>h1')
     $('h1.title').append(searchTmpl)
