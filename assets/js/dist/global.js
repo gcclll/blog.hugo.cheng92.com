29f9fc8f6bbb83b382ea1fde77119092c6ae6bca
gcclll <gccll.love@gmail.com>
2022-03-02 10:21:03 +0800

    2022-03-02 10:21:03

--- assets/js/dist/global.js
+++ assets/js/dist/global.js
@@ -59,7 +59,86 @@ $(function () {
     $('#postamble').css({
       width: '100%',
       textAlign: 'center'
+    }); // 搜索组件
+
+    var _Search = Vue.defineComponent({
+      template: "\n        <el-dialog v-model=\"dialogVisible\" @open=\"clean\" @close=\"clean\" title=\"\u5168\u6587(\u7AD9)\u641C\u7D22\">\n          <el-input autofocus v-model=\"search\" placeholder=\"\u8BF7\u8F93\u5165\u641C\u7D22\u5185\u5BB9(\u6682\u53EA\u652F\u6301\u6807\u9898\u3001\u94FE\u63A5\u3001\u951A\u70B9)\">\n            <template #prepend>\n              <el-select v-model=\"scope\" placeholder=\"Select\" style=\"width:80px\">\n                <el-option label=\"\u672C\u6587\" value=\"1\"/>\n                <el-option label=\"\u5168\u7AD9\" value=\"2\"/>\n              </el-select>\n            </template>\n            <template #append><img class=\"my-search-icon\" src=\"/assets/img/search.svg\"></template>\n          </el-input>\n          <ul class=\"search-list\" style=\"max-height:500px;overflow-y:scroll;text-align:left\">\n            <li v-for=\"(result, i) in filterResults\" :key=\"result.value\" @click=\"locate(result.link)\">\n              <div class=\"result-value\" v-html=\"highlight(result.value)\"></div>\n              <div class=\"result-tags\">\n                <el-tag v-if=\"!isCurrentPage(result.file)\" effect=\"dark\" type=\"info\">{{result.file}}</el-tag>\n              </div>\n            </li>\n          </ul>\n        </el-dialog>",
+      setup: function setup() {
+        var state = Vue.reactive({
+          results: [],
+          filterResults: [],
+          search: '',
+          dialogVisible: false,
+          scope: '2' // 1 - 本文, 2 - 全站
+
+        });
+        Vue.onMounted(function () {
+          state.results = state.scope === '1' ? cached.current : cached.whole;
+          $(document.body).on('keydown', keydownHandler);
+        });
+
+        function keydownHandler(e) {
+          if (e.metaKey && e.keyCode === 75) {
+            state.dialogVisible = true;
+          }
+        }
+
+        Vue.onUnmounted(function () {
+          $(document.body).off('keydown', keydownHandler);
+        });
+        Vue.watch(function () {
+          return state.scope;
+        }, function (val) {
+          return state.results = val === '1' ? cached.current : cached.whole;
+        });
+        Vue.watch(function () {
+          return state.search;
+        }, function (newVal) {
+          if (newVal) {
+            _querySearch(newVal, function (results) {
+              return state.filterResults = results;
+            }, state.results);
+          } else {
+            state.filterResults = [];
+          }
+        });
+
+        var clean = function clean() {
+          state.filterResults = [];
+          state.search = '';
+        };
+
+        return _objectSpread(_objectSpread({}, Vue.toRefs(state)), {}, {
+          clean: clean,
+          // 高亮匹配内容
+          highlight: function highlight(value) {
+            var words = state.search.split(' ');
+            words.forEach(function (word) {
+              value = value.replace(new RegExp("".concat(word), 'gi'), "<span class=\"hl-word\">".concat(word, "</span>"));
+            });
+            return value;
+          },
+          isCurrentPage: function isCurrentPage(file) {
+            return new RegExp("".concat(file, "$")).test(location.pathname);
+          },
+          locate: function locate(link) {
+            location.href = link;
+            clean();
+            state.dialogVisible = false;
+          },
+          querySearch: function querySearch(qs, cb) {
+            return _querySearch(qs, cb, state.results);
+          },
+          handleSelect: function handleSelect(item) {
+            if (item.link) {
+              location.href = item.href;
+              state.search = '';
+            }
+          }
+        });
+      }
     }); // 收集所有标题(id包含 'outline-container-' 且以它开头的 div)
+
 
     $(searchTmpl).insertAfter('h1.title');
     $("<div id=\"vue-toc\"></div>").insertAfter('#search');
@@ -92,82 +171,26 @@ $(function () {
       $(this).css('background-color', 'rgba(35, 39, 46, .1)');
     }
   });
-  $('#postamble .author').append($('<span class="follows"><a href="https://www.github.com/gcclll?tab=followers">' + '<img src="https://img.shields.io/github/followers/gcclll?style=social"></a></span>'));
+  $('#postamble .author').append($('<span class="follows"><a href="https://www.github.com/gcclll?tab=followers">' + '<img src="https://img.shields.io/github/followers/gcclll?style=social"></a></span>')); // search ///////////////////////////////////////////////////////////////////
+
   var app = Vue.createApp({
-    template: "\n\n<el-autocomplete\n  v-model=\"search\"\n  :fetch-suggestions=\"querySearch\"\n  :trigger-on-focus=\"false\"\n  class=\"inline-input search-input\"\n  placeholder=\"\u5168\u6587\u6216\u672C\u6587\u4E2D\u641C\u7D22...\"\n  @select=\"handleSelect\"\n>\n  <template #suffix>\n    <img class=\"command-k\" src=\"/assets/img/command.svg\"/><span class=\"command-k\">K</span>\n  </template>\n</el-autocomplete>\n<el-dialog v-model=\"dialogVisible\" @open=\"clean\" @close=\"clean\" title=\"\u5168\u6587(\u7AD9)\u641C\u7D22\">\n<el-input autofocus v-model=\"search\" placeholder=\"\u8BF7\u8F93\u5165\u641C\u7D22\u5185\u5BB9(\u6682\u53EA\u652F\u6301\u6807\u9898\u3001\u94FE\u63A5\u3001\u951A\u70B9)\">\n  <template #prepend>\n    <el-select v-model=\"scope\" placeholder=\"Select\" style=\"width:80px\">\n      <el-option label=\"\u672C\u6587\" value=\"1\"/>\n      <el-option label=\"\u5168\u7AD9\" value=\"2\"/>\n    </el-select>\n  </template>\n  <template #append><img class=\"my-search-icon\" src=\"/assets/img/search.svg\"></template>\n</el-input>\n<ul class=\"search-list\" style=\"max-height:500px;overflow-y:scroll;text-align:left\">\n  <li v-for=\"(result, i) in filterResults\" :key=\"result.value\" @click=\"locate(result.link)\">\n    <div class=\"result-value\" v-html=\"highlight(result.value)\"></div>\n    <div class=\"result-tags\">\n      <el-tag v-if=\"!isCurrentPage(result.file)\" effect=\"dark\" type=\"info\">{{result.file}}</el-tag>\n    </div>\n  </li>\n</ul>\n</el-dialog>\n",
+    template: "\n<el-autocomplete\n  v-model=\"search\"\n  :fetch-suggestions=\"querySearch\"\n  :trigger-on-focus=\"false\"\n  class=\"inline-input search-input\"\n  placeholder=\"\u5168\u6587\u6216\u672C\u6587\u4E2D\u641C\u7D22...\"\n  @select=\"handleSelect\"\n>\n  <template #suffix>\n    <img class=\"command-k\" src=\"/assets/img/command.svg\"/><span class=\"command-k\">K</span>\n  </template>\n</el-autocomplete>\n<search/>\n",
+    components: {
+      Search: Search
+    },
     setup: function setup() {
-      var state = Vue.reactive({
-        results: [],
-        filterResults: [],
-        search: '',
-        dialogVisible: false,
-        scope: '2' // 1 - 本文, 2 - 全站
-
-      });
-      Vue.onMounted(function () {
-        state.results = state.scope === '1' ? cached.current : cached.whole;
-        $(document.body).on('keydown', keydownHandler);
-      });
-
-      function keydownHandler(e) {
-        if (e.metaKey && e.keyCode === 75) {
-          state.dialogVisible = true;
-        }
-      }
-
-      Vue.onUnmounted(function () {
-        $(document.body).off('keydown', keydownHandler);
-      });
-      Vue.watch(function () {
-        return state.scope;
-      }, function (val) {
-        return state.results = val === '1' ? cached.current : cached.whole;
-      });
-      Vue.watch(function () {
-        return state.search;
-      }, function (newVal) {
-        if (newVal) {
-          _querySearch(newVal, function (results) {
-            return state.filterResults = results;
-          }, state.results);
-        } else {
-          state.filterResults = [];
-        }
-      });
-
-      var clean = function clean() {
-        state.filterResults = [];
-        state.search = '';
-      };
-
-      return _objectSpread(_objectSpread({}, Vue.toRefs(state)), {}, {
-        clean: clean,
-        // 高亮匹配内容
-        highlight: function highlight(value) {
-          var words = state.search.split(' ');
-          words.forEach(function (word) {
-            value = value.replace(new RegExp("".concat(word), 'gi'), "<span class=\"hl-word\">".concat(word, "</span>"));
-          });
-          return value;
-        },
-        isCurrentPage: function isCurrentPage(file) {
-          return new RegExp("".concat(file, "$")).test(location.pathname);
-        },
-        locate: function locate(link) {
-          location.href = link;
-          clean();
-          state.dialogVisible = false;
-        },
+      var search = ref('');
+      return {
         querySearch: function querySearch(qs, cb) {
-          return _querySearch(qs, cb, state.results);
+          return _querySearch(qs, cb, deduped);
         },
         handleSelect: function handleSelect(item) {
           if (item.link) {
+            search.value = '';
             location.href = item.href;
-            state.search = '';
           }
         }
-      });
+      };
     }
   });
   app.use(ElementPlus, ElementPlusOptions).mount('#search');
--- assets/js/src/global.js
+++ assets/js/src/global.js
@@ -70,6 +70,109 @@ $(function () {
       width: '100%',
       textAlign: 'center'
     })
+
+    // 搜索组件
+    const Search = Vue.defineComponent({
+      template: `
+        <el-dialog v-model="dialogVisible" @open="clean" @close="clean" title="全文(站)搜索">
+          <el-input autofocus v-model="search" placeholder="请输入搜索内容(暂只支持标题、链接、锚点)">
+            <template #prepend>
+              <el-select v-model="scope" placeholder="Select" style="width:80px">
+                <el-option label="本文" value="1"/>
+                <el-option label="全站" value="2"/>
+              </el-select>
+            </template>
+            <template #append><img class="my-search-icon" src="/assets/img/search.svg"></template>
+          </el-input>
+          <ul class="search-list" style="max-height:500px;overflow-y:scroll;text-align:left">
+            <li v-for="(result, i) in filterResults" :key="result.value" @click="locate(result.link)">
+              <div class="result-value" v-html="highlight(result.value)"></div>
+              <div class="result-tags">
+                <el-tag v-if="!isCurrentPage(result.file)" effect="dark" type="info">{{result.file}}</el-tag>
+              </div>
+            </li>
+          </ul>
+        </el-dialog>`,
+      setup() {
+        const state = Vue.reactive({
+          results: [],
+          filterResults: [],
+          search: '',
+          dialogVisible: false,
+          scope: '2' // 1 - 本文, 2 - 全站
+        })
+
+        Vue.onMounted(() => {
+          state.results = state.scope === '1' ? cached.current : cached.whole
+          $(document.body).on('keydown', keydownHandler)
+        })
+
+        function keydownHandler(e) {
+          if (e.metaKey && e.keyCode === 75) {
+            state.dialogVisible = true
+          }
+        }
+        Vue.onUnmounted(() => {
+          $(document.body).off('keydown', keydownHandler)
+        })
+
+        Vue.watch(
+          () => state.scope,
+          (val) => (state.results = val === '1' ? cached.current : cached.whole)
+        )
+
+        Vue.watch(
+          () => state.search,
+          (newVal) => {
+            if (newVal) {
+              querySearch(
+                newVal,
+                (results) => (state.filterResults = results),
+                state.results
+              )
+            } else {
+              state.filterResults = []
+            }
+          }
+        )
+
+        const clean = () => {
+          state.filterResults = []
+          state.search = ''
+        }
+
+        return {
+          ...Vue.toRefs(state),
+          clean,
+          // 高亮匹配内容
+          highlight(value) {
+            const words = state.search.split(' ')
+            words.forEach((word) => {
+              value = value.replace(
+                new RegExp(`${word}`, 'gi'),
+                `<span class="hl-word">${word}</span>`
+              )
+            })
+            return value
+          },
+          isCurrentPage(file) {
+            return new RegExp(`${file}$`).test(location.pathname)
+          },
+          locate(link) {
+            location.href = link
+            clean()
+            state.dialogVisible = false
+          },
+          querySearch: (qs, cb) => querySearch(qs, cb, state.results),
+          handleSelect(item) {
+            if (item.link) {
+              location.href = item.href
+              state.search = ''
+            }
+          }
+        }
+      }
+    })
     // 收集所有标题(id包含 'outline-container-' 且以它开头的 div)
     $(searchTmpl).insertAfter('h1.title')
     $(`<div id="vue-toc"></div>`).insertAfter('#search')
@@ -121,9 +224,9 @@ $(function () {
     )
   )
 
+  // search ///////////////////////////////////////////////////////////////////
   const app = Vue.createApp({
     template: `
-
 <el-autocomplete
   v-model="search"
   :fetch-suggestions="querySearch"
@@ -136,101 +239,19 @@ $(function () {
     <img class="command-k" src="/assets/img/command.svg"/><span class="command-k">K</span>
   </template>
 </el-autocomplete>
-<el-dialog v-model="dialogVisible" @open="clean" @close="clean" title="全文(站)搜索">
-<el-input autofocus v-model="search" placeholder="请输入搜索内容(暂只支持标题、链接、锚点)">
-  <template #prepend>
-    <el-select v-model="scope" placeholder="Select" style="width:80px">
-      <el-option label="本文" value="1"/>
-      <el-option label="全站" value="2"/>
-    </el-select>
-  </template>
-  <template #append><img class="my-search-icon" src="/assets/img/search.svg"></template>
-</el-input>
-<ul class="search-list" style="max-height:500px;overflow-y:scroll;text-align:left">
-  <li v-for="(result, i) in filterResults" :key="result.value" @click="locate(result.link)">
-    <div class="result-value" v-html="highlight(result.value)"></div>
-    <div class="result-tags">
-      <el-tag v-if="!isCurrentPage(result.file)" effect="dark" type="info">{{result.file}}</el-tag>
-    </div>
-  </li>
-</ul>
-</el-dialog>
+<search/>
 `,
+    components: {
+      Search
+    },
     setup() {
-      const state = Vue.reactive({
-        results: [],
-        filterResults: [],
-        search: '',
-        dialogVisible: false,
-        scope: '2' // 1 - 本文, 2 - 全站
-      })
-
-      Vue.onMounted(() => {
-        state.results = state.scope === '1' ? cached.current : cached.whole
-        $(document.body).on('keydown', keydownHandler)
-      })
-
-      function keydownHandler(e) {
-        if (e.metaKey && e.keyCode === 75) {
-          state.dialogVisible = true
-        }
-      }
-      Vue.onUnmounted(() => {
-        $(document.body).off('keydown', keydownHandler)
-      })
-
-      Vue.watch(
-        () => state.scope,
-        (val) => (state.results = val === '1' ? cached.current : cached.whole)
-      )
-
-      Vue.watch(
-        () => state.search,
-        (newVal) => {
-          if (newVal) {
-            querySearch(
-              newVal,
-              (results) => (state.filterResults = results),
-              state.results
-            )
-          } else {
-            state.filterResults = []
-          }
-        }
-      )
-
-      const clean = () => {
-        state.filterResults = []
-        state.search = ''
-      }
-
+      const search = ref('')
       return {
-        ...Vue.toRefs(state),
-        clean,
-        // 高亮匹配内容
-        highlight(value) {
-          const words = state.search.split(' ')
-          words.forEach((word) => {
-            value = value.replace(
-              new RegExp(`${word}`, 'gi'),
-              `<span class="hl-word">${word}</span>`
-            )
-          })
-          return value
-        },
-        isCurrentPage(file) {
-          return new RegExp(`${file}$`).test(location.pathname)
-        },
-        locate(link) {
-          location.href = link
-          clean()
-          state.dialogVisible = false
-        },
-        querySearch: (qs, cb) => querySearch(qs, cb, state.results),
+        querySearch: (qs, cb) => querySearch(qs, cb, deduped),
         handleSelect(item) {
           if (item.link) {
+            search.value = ''
             location.href = item.href
-            state.search = ''
           }
         }
       }
