70cefb4c4ae8a26fdc95a921b5df07af96d09cef
gcclll <gccll.love@gmail.com>
2022-03-09 14:30:26 +0800

    2022-03-09 14:30:26

--- src/apps/search.js
+++ src/apps/search.js
@@ -20,7 +20,7 @@ export default function loadSearchApp() {
   Vue.createApp({
     template: `
         <el-input
-          v-if="isHome"
+          v-if="false"
           class="inline-input search-input"
           v-model="search" placeholder="搜索本文(Alt/Cmd+K 全站搜索)">
           <template #suffix><search-suffix /></template>
@@ -39,7 +39,7 @@ export default function loadSearchApp() {
                   <svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                 </div>
                 <div class="hit-content-wrap">
-                  <span class="hit-title">{{item.title}}</span>
+                  <span class="hit-title" v-html="item.title"></span>
                   <span class="hit-path">{{item.link}}</span>
                 </div>
                 <div class="hit-action">
