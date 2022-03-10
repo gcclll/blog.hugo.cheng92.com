42a4d8cf661b089a38c70167a8f8f3d193f45366
gcclll <gccll.love@gmail.com>
2022-03-10 11:41:46 +0800

    2022-03-10 11:41:46

--- src/components/HomeToc.js
+++ src/components/HomeToc.js
@@ -11,11 +11,12 @@ export default Vue.defineComponent({
   <el-menu class="el-toc-menu" v-if="activeIndex===types.TYPE_ARCHIVES">
     <el-menu-item-group v-if="listType===types.TYPE_ARCHIVES" v-for="(list, month) in listData" :key="month" :title="month">
       <el-menu-item v-for="(page, i) in list" :index="i+''" :key="page.timestamp">
-      <span class="date">{{page.date}}</span>
-      <span class="title">
-        <a :href="page.url" v-html="page.title"></a>
-      </span>
-      <div/>
+      <div class="date-title">
+        <span class="date">{{page.date}}</span>
+        <span class="title">
+          <a :href="page.url" v-html="page.title"></a>
+        </span>
+      </div>
       <div class="tags">
         <el-tag size="small" v-for="cat in page.category" :key="cat" type="success">{{cat}}</el-tag>
         <el-tag size="small" v-for="tag in page.tags" :key="tag">{{tag}}</el-tag>
