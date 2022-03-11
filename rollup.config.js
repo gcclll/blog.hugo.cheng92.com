180d176c0f77635fe4c3d269eb24c6f5557cd9a8
gcclll <gccll.love@gmail.com>
2022-03-11 22:38:27 +0800

    2022-03-11 22:38:26

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
-// import injectProcessEnv from 'rollup-plugin-inject-process-env'
+import injectProcessEnv from 'rollup-plugin-inject-process-env'
 import { terser } from 'rollup-plugin-terser'
 import scss from 'rollup-plugin-scss'
 
@@ -13,17 +13,18 @@ export default {
   },
   plugins: [
     babel({ babelHelpers: 'bundled' }),
-    // injectProcessEnv({
-    //   NODE_ENV: 'production',
-    //   SOME_OBJECT: { one: 1, two: [1, 2], three: '3' },
-    //   UNUSED: null
-    // }),
+
     nodeResolve(),
     terser(),
     // https://www.npmjs.com/package/rollup-plugin-scss
     scss({
       output: './assets/css/global.css',
       sourceMap: true
+    }),
+    injectProcessEnv({
+      NODE_ENV: 'production',
+      SOME_OBJECT: { one: 1, two: [1, 2], three: '3' },
+      UNUSED: null
     })
   ],
   watch: {
