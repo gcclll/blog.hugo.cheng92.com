fea34ff8508d0e39cf273fb850313bf6a869c4d4
gcclll <gccll.love@gmail.com>
2022-03-16 11:39:57 +0800

    2022-03-16 11:39:57

--- apps/vue/src/App.vue
+++ apps/vue/src/App.vue
@@ -1,24 +1,23 @@
 <template>
-    <BaseHeader />
-    <div style="display: flex">
-        <BaseSide />
-        <div>
-            <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" />
-            <Markdown />
-            <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
-        </div>
+  <BaseHeader />
+  <div style="display: flex">
+    <BaseSide />
+    <div>
+      <Markdown />
+      <HelloWorld msg="Hello 111 xxx xxVue 3.0 + Element Plus + Vite" />
     </div>
+  </div>
 </template>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 .element-plus-logo {
-    width: 50%;
+  width: 50%;
 }
 </style>
---
+++ scripts/watch.js
@@ -0,0 +1,37 @@
+/** jsx?|tsx? file header */
+
+const { execSync } = require('child_process')
+const fs = require('fs')
+
+let code = 0
+fs.watch(
+  './apps/',
+  {
+    recursive: true
+  },
+  (e, filename) => {
+    if (
+      filename &&
+      e === 'change' &&
+      /(vue|js|ts|md|scss|css)$/.test(filename)
+    ) {
+      console.log(`[${filename}] changed`)
+
+      if (code !== 0) {
+        console.warn('process is building or some error occured...')
+        return
+      }
+      code = -1
+      try {
+        execSync(
+          'cd ~/github/mine/blog.cheng92.com/apps/vue && pnpm run build && cd ../.. && npm run pub'
+        )
+        code = 0
+      } catch (e) {
+        code = -1
+        console.warn(e)
+      }
+    }
+  }
+)
+console.log('Listening apps change...')
