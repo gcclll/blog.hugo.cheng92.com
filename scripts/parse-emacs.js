f57c960f2c964a1567631722536d32515d2ef997
gcclll <gccll.love@gmail.com>
2022-05-29 11:43:44 +0800

    2022-05-29 11:43:43

--- scripts/parse-emacs.js
+++ scripts/parse-emacs.js
@@ -1,7 +1,6 @@
 // 解析 ~/.emacs.d/site-lisp/config/init-general-key.el 中所有的 bindingkeys
 // 到 /assets/js/stats/ 中
 // 在 [[https://blog.cheng92.com/posts/emacs_all_keybindings.html][Emacs All Keybindings]] 
-
 const fs = require('fs')
 
 fs.readFile(`${process.env.HOME}/.emacs.d/site-lisp/config/init-general-key.el`, {
@@ -11,21 +10,21 @@ fs.readFile(`${process.env.HOME}/.emacs.d/site-lisp/config/init-general-key.el`,
     console.error(err)
     return
   }
-
+ 
   const squares = data.match(/;; {{[^(}})]+;; }}/ig) || []
   if (squares.length) {
     const result = {}
     squares.forEach(text => {
       // ;; {{ hint --
-      // ...
-      // ;; }}
-      const trimed = text.trim()
-      // 请确保是以正确的格式写的 
-      trimed.match(/;; {{ ([^#]+) ##/ig)[0]
-      const title = RegExp.$1
+       // ...
+       // ;; }}
+       const trimed = text.trim()
+       // 请确保是以正确的格式写的 
+       trimed.match(/;; {{ ([^#]+) ##/ig)[0]
+       const title = RegExp.$1
       const keyList = trimed.split('\n').filter(it => {
         return !/^;;/.test(it.trim())
-      }).map(it => it.trim())
+      }).map(it => it.trim().replace(/<([a-z0-9]+)>/i, "$1"))
 
       result[title] = keyList
     })
